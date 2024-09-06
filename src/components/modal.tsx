8001a884de32197d0bf81911bc6f0878d00ad75a
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2024-09-06 12:32:54 +0530

    added css to modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -3,13 +3,13 @@ import { postUsers } from "../api/users";
 import { User, Address, Company, DisplayUser} from "../types";
 interface prop {
     users: DisplayUser[],
-    setUsers: React.Dispatch<React.SetStateAction<DisplayUser[]>>
+    setUsers: React.Dispatch<React.SetStateAction<DisplayUser[]>>,
     onClose : () => void
 }
 
-export const Modal = (prop : prop) => {
+export const Modal = (prop: prop) => {
   const [formData, setFormData] = useState<User>({
-    id : 0,
+    id: 0,
     name: "",
     username: "",
     email: "",
@@ -41,7 +41,6 @@ export const Modal = (prop : prop) => {
     if (parent === "address" || parent === "company") {
       setFormData((prevData) => {
         const parentObject = prevData[parent as keyof User] as Address | Company;
-
         const updatedParent = {
           ...parentObject,
           [child as keyof Address | keyof Company]: value,
@@ -58,7 +57,6 @@ export const Modal = (prop : prop) => {
         [name as keyof User]: value,
       }));
     }
-    console.log(formData)
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -87,50 +85,51 @@ export const Modal = (prop : prop) => {
         bs: formData.company.bs,
       },
     });
-    if(user)
-    {
-        const displayUser  : DisplayUser = {
-            id: user.id,
-            name: user.name,
-            username: user.username,
-            email: user.email    
-        } 
-        prop.setUsers((prevData) => ([...prevData,displayUser]));
-        prop.onClose();
+
+    if (user) {
+      const displayUser: DisplayUser = {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+      };
+      prop.setUsers((prevData) => [...prevData, displayUser]);
+      prop.onClose();
     }
-    
-    
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h1>Basic Details</h1>
-          <input name="name" placeholder="Name" value={formData.name} onChange={handleChange} />
-          <input name="username" placeholder="Username" value={formData.username} onChange={handleChange} />
-          <input name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
-          <input name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} />
-          <input name="website" placeholder="Website" value={formData.website} onChange={handleChange} />
-        </div>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={prop.onClose}>X</button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <h1>Basic Details</h1>
+            <input name="name" placeholder="Name" value={formData.name} onChange={handleChange} />
+            <input name="username" placeholder="Username" value={formData.username} onChange={handleChange} />
+            <input name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
+            <input name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} />
+            <input name="website" placeholder="Website" value={formData.website} onChange={handleChange} />
+          </div>
 
-        <div>
-          <h1>Address</h1>
-          <input name="address.street" placeholder="Street" value={formData.address.street} onChange={handleChange} />
-          <input name="address.suite" placeholder="Suite" value={formData.address.suite} onChange={handleChange} />
-          <input name="address.city" placeholder="City" value={formData.address.city} onChange={handleChange} />
-          <input name="address.zipcode" placeholder="Zip Code" value={formData.address.zipcode} onChange={handleChange} />
-        </div>
+          <div>
+            <h1>Address</h1>
+            <input name="address.street" placeholder="Street" value={formData.address.street} onChange={handleChange} />
+            <input name="address.suite" placeholder="Suite" value={formData.address.suite} onChange={handleChange} />
+            <input name="address.city" placeholder="City" value={formData.address.city} onChange={handleChange} />
+            <input name="address.zipcode" placeholder="Zip Code" value={formData.address.zipcode} onChange={handleChange} />
+          </div>
 
-        <div>
-          <h1>Company</h1>
-          <input name="company.name" placeholder="Name" value={formData.company.name} onChange={handleChange} />
-          <input name="company.catchPhrase" placeholder="Catch Phrase" value={formData.company.catchPhrase} onChange={handleChange} />
-          <input name="company.bs" placeholder="Bs" value={formData.company.bs} onChange={handleChange} />
-        </div>
+          <div>
+            <h1>Company</h1>
+            <input name="company.name" placeholder="Name" value={formData.company.name} onChange={handleChange} />
+            <input name="company.catchPhrase" placeholder="Catch Phrase" value={formData.company.catchPhrase} onChange={handleChange} />
+            <input name="company.bs" placeholder="Bs" value={formData.company.bs} onChange={handleChange} />
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
